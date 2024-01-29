f4d263bfc999cf6e7fc848c0e741b08be43db427
aydeetech <ayomidefaseun1267@gmail.com>
2024-01-29 12:25:40 +0100

    first commit

---
+++ middleware/auth.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken")
+
+const authentication = (req,res,next) => {
+    const authHeader = req.headers.authorization
+
+    try {
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            return res.status(401).json({err: "Invalid Authentication"})
+        }
+
+        const token = authHeader.split(" ")[1]
+
+        const payload = jwt.verify(token, process.env.SECRET_KEY)
+        req.user = {userId: payload.userId, role: payload.role}
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.status(403).json({err: "Authentication failed"})
+    }
+}
+
+const requirePermission = (role) => {
+    return (req,res, next) => {
+        if (req.user.role !== role) {
+            return res.status(403).json({err: "Unauthorized to perform this action"})
+        }
+        next();
+    }
+}
+
+module.exports = {authentication, requirePermission}
